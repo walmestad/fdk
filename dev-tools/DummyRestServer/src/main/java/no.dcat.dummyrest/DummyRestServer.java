eff6a83a7bf9d9285ba48be0472bba20a007d04e
exttas <exttas@breg.no>
2018-05-29 09:48:58 +0200

    filer som manglet i forrige push

---
+++ dev-tools/DummyRestServer/src/main/java/no.dcat.dummyrest/DummyRestServer.java
@@ -0,0 +1,59 @@
+package no.dcat.dummyrest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Random;
+
+@RestController
+public class DummyRestServer {
+
+    private static Logger logger = LoggerFactory.getLogger(DummyRestServer.class);
+
+    @RequestMapping(value = "/subject/{uri}", produces = "text/turtle")
+    public @ResponseBody
+    ResponseEntity getSubject(@PathVariable("uri") String uri) {
+        String begrep = "# Hovedenhet\n" +
+                "# https://data-david.github.io/Begrep/begrep/Hovedenhet\n" +
+                "@prefix rdf:\t<http://www.w3.org/1999/02/22-rdf-syntax-ns#> .\n" +
+                "@prefix rdfs:\t<http://www.w3.org/2000/01/rdf-schema#> .\n" +
+                "@prefix owl:\t<http://www.w3.org/2002/07/owl#> .\n" +
+                "@prefix dct:\t<http://purl.org/dc/terms/> .\n" +
+                "@prefix skos: \t<http://www.w3.org/2004/02/skos/core#> .\n" +
+                "@prefix xsd:\t<http://www.w3.org/2001/XMLSchema#> .\n" +
+                "@prefix :\t<https://data-david.github.io/Begrep/begrep/> .\n" +
+                "\n" +
+                ":Hovedenhet\n" +
+                "\t\ta skos:Concept ;\n" +
+                "\t\tskos:prefLabel \"hovedenhet\"@no ;\n" +
+                "\t\tskos:definition \"enhet på øverste nivå i registreringsstrukturen i Enhetsregisteret\"@no ;\n" +
+                "\t\tskos:note \"Enkeltpersonforetak, foreninger, selskap, sameier og andre som er registrert i Enhetsregisteret. Identifiseres med organisasjonsnummer.\"@no ;\n" +
+                "\t\tdct:source <https://jira.brreg.no/browse/BEGREP-226> .";
+        begrep = begrep.replace("Hovedenhet",uri);
+        logger.info(begrep);
+
+        ResponseEntity responseEntity = new ResponseEntity(begrep,null, HttpStatus.OK);
+        return responseEntity;
+//        Random random = new Random();
+//
+//        int latency = random.nextInt(3000);
+//        boolean fail = random.nextInt(5) == 1;
+//        try {
+//            Thread.sleep(latency);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+//        logger.info(begrep);
+//
+//        if(fail) {
+//            return new ResponseEntity(HttpStatus.NOT_FOUND);
+//        }
+//        else {
+//            return responseEntity;
+//        }
+    }
+}
+
---
+++ dev-tools/DummyRestServer/src/main/java/no.dcat.dummyrest/DummyRestServerApplication.java
@@ -0,0 +1,12 @@
+package no.dcat.dummyrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DummyRestServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DummyRestServerApplication.class, args);
+    }
+
+}
